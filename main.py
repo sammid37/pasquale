e750211150f9286191c3f20700cd9449125c3417
sammid37 <samanthadmedeiros37@gmail.com>
2024-04-19 15:53:08 -0300

    🍎 Main

---
+++ main.py
@@ -0,0 +1,11 @@
+# Construção de Compiladores 
+# Arquivo principal 
+# Enthony e Samantha
+
+print("Pasquale Compiler")
+print("Compilador da Língua Portuguesa")
+
+# TODO: chamada do Léxico
+# TODO: chamada do Sintático
+# TODO: disparar requisições da frase original e novas frases para o Google
+# TODO: exibir snippets a partir de claims
