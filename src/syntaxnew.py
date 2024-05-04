17b2dd4ebe4b281c85969ff09f3ed728ea4519f6
EnthonyMiguel <enthonymiguel@gmail.com>
2024-05-04 19:38:14 -0300

    Requisição dos Snippets e Atualização da Grmática!
    Co-authored-by: Samantha Medeiros <samanthadmedeiros37@gmail.com>

--- src/syntaxnew.py
+++ src/syntaxnew.py
@@ -7,25 +7,25 @@ class SyntaxNew:
     self.tokens = []
 
   def my_grammar(self):
-    return CFG.fromstring("""
-       Texto -> Sentenca PONTUACAO_FINAL | Sentenca PONTUACAO_FINAL Texto
-            Sentenca -> SintagmaNominal SintagmaVerbal | SintagmaVerbal SintagmaNominal | SintagmaNominal SintagmaNominal | SintagmaNominal SintagmaVerbal SintagmaNominal | SintagmaVerbal SintagmaNominal SintagmaNominal | SintagmaVerbal SintagmaNominal SintagmaNominal SintagmaNominal | SintagmaNominal SintagmaVerbal Conjuncao SintagmaNominal SintagmaVerbal
-            SintagmaNominal -> Substantivo | Artigo Substantivo | Pronome Substantivo | Substantivo Adjetivo | Pronome Adjetivo Substantivo | Substantivo Adjetivo Adjetivo | Pronome Adjetivo Adjetivo Substantivo | SubstantivoProprio | Preposicao SubstantivoProprio | Preposicao Substantivo | Preposicao Substantivo Preposicao Substantivo | Preposicao Substantivo Preposicao SubstantivoProprio | Preposicao Artigo Substantivo
-            SintagmaVerbal -> Verbo | Verbo Adverbio | Verbo Adjetivo | Verbo Adjetivo Adverbio | Verbo Adverbio Adjetivo | Verbo Adverbio Adjetivo Adverbio | VerboAuxiliar | VerboAuxiliar Verbo Adverbio | VerboAuxiliar Verbo Adjetivo | VerboAuxiliar Verbo  Adjetivo Adverbio | VerboAuxiliar Verbo Adverbio Adjetivo | VerboAuxiliar Verbo Adverbio Adjetivo Adverbio
-            Artigo -> 'DET'
-            Substantivo -> 'NOUN'
-            Pronome -> 'PRON'
-            Adjetivo -> 'ADJ'                     
-            Verbo -> 'VERB' | 'AUX' | 'AUX' 'VERB'
-            Adverbio -> 'ADV'
-            Preposicao -> 'ADP'
-            Conjuncao -> 'CCONJ'
-            Interjeicao -> 'INTJ'
-            Numeral -> 'NUM'
-            VerboAuxiliar -> 'AUX'
-            SubstantivoProprio -> 'PROPN'
-            PONTUACAO_FINAL -> 'PUNCT'
-  """)
+    return CFG.fromstring(
+      """Texto -> Sentenca PONTUACAO_FINAL | Sentenca PONTUACAO_FINAL Texto
+      Sentenca -> SintagmaNominal SintagmaVerbal | SintagmaVerbal SintagmaNominal | SintagmaNominal | SintagmaNominal SintagmaVerbal SintagmaNominal | SintagmaVerbal SintagmaNominal | SintagmaVerbal | SintagmaNominal SintagmaVerbal Conjuncao SintagmaNominal SintagmaVerbal | SintagmaNominal  
+      SintagmaNominal -> Determinante Substantivo | Determinante Substantivo Adjetivo | Pronome | Determinante SubstantivoProprio | Determinante SubstantivoProprio Adjetivo | Determinante SubstantivoProprio Conjuncao Determinante SubstantivoProprio | Determinante Substantivo Conjuncao Determinante Substantivo | Preposicao Substantivo | Preposicao SubstantivoProprio | Determinante 
+      SintagmaVerbal -> Verbo | SintagmaNominal | Verbo Adjetivo| Verbo Adverbio | Verbo Pronome | Verbo Substantivo Numeral | Verbo Determinante Substantivo Adjetivo | Verbo Determinante Substantivo Adjetivo Sintagma Nominal | Verbo Determinante Substantivo Preposicao Substantivo Averbio | Verbo SintagmaNominal Preposicao Determinante Substantivo | Verbo SintagmaNominal Preposicao Determinante Substantivo Adjetivo | Adverbio Verbo Determinante Substantivo Preposicao Substantivo 
+      Determinante -> 'DET'
+      Substantivo -> 'NOUN'
+      Pronome -> 'PRON'
+      Adjetivo -> 'ADJ'                     
+      Verbo -> 'VERB' | 'AUX' | 'AUX' 'VERB'
+      Adverbio -> 'ADV'
+      Preposicao -> 'ADP'
+      Conjuncao -> 'CCONJ'
+      Interjeicao -> 'INTJ'
+      Numeral -> 'NUM'
+      VerboAuxiliar -> 'AUX'
+      SubstantivoProprio -> 'PROPN'
+      Numeral -> 'NUM'
+      PONTUACAO_FINAL -> 'PUNCT'""")
 
   def display_tree(self, parser, token_list):
     for tree in parser.parse(token_list):
@@ -42,11 +42,57 @@ class SyntaxNew:
 
     print("Ordem das classes gramaticiais: ")
     print(grammar_list)
-    for tree in parser.parse(grammar_list):
-      tree.pretty_print()
+    # for tree in parser.parse(grammar_list):
+    #   tree.pretty_print()
     tree = self.display_tree(parser, grammar_list)
     if tree:
-       print(colored("✅Análise sintática concluída!", "green"))
+       print(colored("✅ Análise sintática concluída!", "green"))
     else:
         print(colored("⚠ Análise sintática finalizada com erro.","red"))
     #TODO Try Catch para capturar exceções de análise sintática
+
+
+
+
+
+
+# Texto -> Sentenca PONTUACAO_FINAL TextoAux
+#         TextoAux -> | Sentenca PONTUACAO_FINAL TextoAux
+#         Sentenca -> SintagmaNominal SintagmaVerbal | SintagmaVerbal SintagmaNominal | SintagmaNominal SintagmaNominal | SintagmaNominal SintagmaVerbal SintagmaNominal | SintagmaVerbal SintagmaNominal SintagmaNominal  | SintagmaVerbal SintagmaNominal SintagmaNominal SintagmaNominal| SintagmaNominal SintagmaVerbal Conjuncao SintagmaNominal SintagmaVerbal
+#         SintagmaNominal -> Substantivo SintagmaNominalAux | Artigo Substantivo SintagmaNominalAux | Pronome Substantivo SintagmaNominalAux | Pronome Adjetivo Substantivo SintagmaNominalAux | SubstantivoProprio SintagmaNominalAux | Preposicao SubstantivoProprio SintagmaNominalAux | Preposicao Substantivo SintagmaNominalAux | Preposicao Substantivo Preposicao Substantivo SintagmaNominalAux | Preposicao Substantivo Preposicao SubstantivoProprio SintagmaNominalAux | Preposicao Artigo Substantivo SintagmaNominalAux
+#         SintagmaNominalAux -> | Adjetivo SintagmaNominalAux | Adjetivo Adjetivo SintagmaNominalAux
+#         SintagmaVerbal -> Verbo SintagmaVerbalAux | VerboAuxiliar Verbo SintagmaVerbalAux
+#         SintagmaVerbalAux -> | Adverbio SintagmaVerbalAux | Adjetivo SintagmaVerbalAux  | Adjetivo Adverbio SintagmaVerbalAux
+#         Artigo -> 'DET'  
+#         Substantivo -> 'NOUN'
+#         Pronome -> 'PRON'
+#         Adjetivo -> 'ADJ'
+#         Verbo -> 'VERB' | 'AUX' 'VERB'
+#         Adverbio -> 'ADV'
+#         Preposicao -> 'ADP'
+#         Conjuncao -> 'CCONJ'
+#         Numeral -> 'NUM'
+#         VerboAuxiliar -> 'AUX'  
+#         SubstantivoProprio -> 'PROPN'
+#         PONTUACAO_FINAL -> 'PUNCT'
+
+# VERSAO ORIGINAL:
+
+# Texto -> Sentenca PONTUACAO_FINAL | Sentenca PONTUACAO_FINAL Texto
+#             Sentenca -> SintagmaNominal SintagmaVerbal | SintagmaVerbal SintagmaNominal | SintagmaNominal SintagmaNominal | SintagmaNominal SintagmaVerbal SintagmaNominal | SintagmaVerbal SintagmaNominal SintagmaNominal | SintagmaVerbal SintagmaNominal SintagmaNominal SintagmaNominal | SintagmaNominal SintagmaVerbal Conjuncao SintagmaNominal SintagmaVerbal
+#             SintagmaNominal -> Substantivo | Artigo Substantivo | Pronome Substantivo | Substantivo Adjetivo | Pronome Adjetivo Substantivo | Substantivo Adjetivo Adjetivo | Pronome Adjetivo Adjetivo Substantivo | SubstantivoProprio | Preposicao SubstantivoProprio | Preposicao Substantivo | Preposicao Substantivo Preposicao Substantivo | Preposicao Substantivo Preposicao SubstantivoProprio | Preposicao Artigo Substantivo | 
+#             SintagmaVerbal -> Verbo | Verbo Adverbio | Verbo Adjetivo | Verbo Adjetivo Adverbio | Verbo Adverbio Adjetivo | Verbo Adverbio Adjetivo Adverbio | VerboAuxiliar | VerboAuxiliar Verbo Adverbio | VerboAuxiliar Verbo Adjetivo | VerboAuxiliar Verbo  Adjetivo Adverbio | VerboAuxiliar Verbo Adverbio Adjetivo | VerboAuxiliar Verbo Adverbio Adjetivo Adverbio
+#             Artigo -> 'DET'
+#             Substantivo -> 'NOUN'
+#             Pronome -> 'PRON'
+#             Adjetivo -> 'ADJ'                     
+#             Verbo -> 'VERB' | 'AUX' | 'AUX' 'VERB'
+#             Adverbio -> 'ADV'
+#             Preposicao -> 'ADP'
+#             Conjuncao -> 'CCONJ'
+#             Interjeicao -> 'INTJ'
+#             Numeral -> 'NUM'
+#             VerboAuxiliar -> 'AUX'
+#             SubstantivoProprio -> 'PROPN'
+#             PONTUACAO_FINAL -> 'PUNCT'
+#   """)
---
+++ src/tests/scraping.py
@@ -0,0 +1,69 @@
+import tabulate
+from typing import List
+from termcolor import colored
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support import expected_conditions as EC
+
+RESULT_STATS = "result-stats"
+ARTICLE_SESSION = "N54PNb BToiNc cvP2Ce"
+SEARCH_TTL = 20
+
+class ElementosNaoEncontradosException(Exception):
+  def __init__(self, message="Os elementos não foram encontrados"):
+    self.message = message
+    super().__init__(self.message)
+
+class Scrapping:
+  def __init__(self, queries:List[str]) -> None:
+    self.queries = queries
+    self.qtd_query_results = []
+    self.snippets_results = {} # dicionário para armazenar a lista de snippets de uma query
+    self.driver = webdriver.Chrome()
+
+  def display_snippets(results: dict) -> None:
+    """Realiza a impressão da query(claim) e seus respectivos snippets 
+    acompanhando da quantidade total de resultados encontrados
+    do claim pesquisado"""
+    print(colored("Resultado da consulta", "magenta"))
+    for q, r in results.items():
+      print(f"\n**Consulta:** {q}")
+      table_data = [
+          ["Quantidade de resultados:", r["quantidade_resultados"]],
+          ["Snippets:", r["snippets"]]
+      ]
+      print(tabulate.tabulate(table_data, tablefmt="fancy"))
+
+  def do_searches(self) -> dict:
+    """Realiza a busca no Google e retorna a quantidade de resultados totais."""
+    for q in self.queries:
+      snippets = []
+      try:
+        self.driver.get("https://www.google.com/search?q=\"" + q + "\"")
+
+        wait = WebDriverWait(self.driver, SEARCH_TTL) # tempo limite para a busca
+        
+        el_qtd_results = wait.until(EC.presence_of_element_located((By.ID, RESULT_STATS)))
+        el_snippets_with_query = self.driver.find_elements(By.CSS_SELECTOR, ".N54PNb.BToiNc")
+
+        for el_snippet_with_query in el_snippets_with_query:
+          snippets.append(el_snippet_with_query.text)
+
+        self.snippets_results[q] = {
+            "quantidade_resultados": el_qtd_results.text,
+            "snippets": snippets
+        }
+      except NoSuchElementException as e:
+        print(colored(f"Elemento não encontrado para a consulta '{q}': {e}"))
+
+    self.driver.quit()
+    return self.snippets_results
+
+# Exemplo de uso
+scraper = Scrapping(["Filme que marca o centenário da Disney", "Ash finalmente ganha a liga pokemon."])
+print(scraper.queries)
+resultados = scraper.do_searches()
+print(type(resultados))
+# scraper.display_snippets(resultados)
