ca9879d3fcaa49156e07818eeaed461796954f59
sammid37 <samanthadmedeiros37@gmail.com>
2024-05-08 17:40:49 -0300

    🍏 Analisador Sintático

--- src/syntaxnew.py
+++ src/syntaxnew.py
@@ -7,9 +7,9 @@ from nltk import CFG, ChartParser
 class SyntaxNew:
     def __init__(self):
         self.tokens = []
-        self.new_sentences = [] # armazena a frase original e suas variações
-
-    def my_grammar(self):
+        
+    def my_grammar(self) -> CFG:
+        """Definição da gramática utilizando NLTK"""
         return CFG.fromstring(
             """Texto -> Sentenca PONTUACAO_FINAL | Sentenca PONTUACAO_FINAL Texto
             Sentenca -> SintagmaNominal SintagmaVerbal | SintagmaVerbal SintagmaNominal | SintagmaNominal | SintagmaVerbal | SintagmaNominal SintagmaVerbal Conjuncao SintagmaNominal SintagmaVerbal | SintagmaVerbal SintagmaNominal Conjuncao SintagmaVerbal SintagmaNominal | SintagmaNominal SintagmaVerbal Conkunção SintagmaNominal SintagmaVerbal | SintagmaNominal SintagmaVerbal Conjuncao SintagmaVerbal | SintagmaNominal SintagmaVerbal Pronome SintagmaVerbal
@@ -30,17 +30,15 @@ class SyntaxNew:
             SubstantivoProprio -> 'PROPN'
             PONTUACAO_FINAL -> 'PUNCT'""")
     
-    def get_new_sentences(self, list_of_sentences) -> List[str]:
-        self.new_sentences = list_of_sentences
-        return self.new_sentences
-
-    def display_tree(self, parser, token_list):
+    def display_tree(self, parser, token_list) -> None:
+        """Exibe a árvore sintática."""
         for tree in parser.parse(token_list):
             print(tree)
             tree.pretty_print()
             return tree
 
     def find_subject(self, word_list, grammar_list):
+        """Encontra o sujeito de uma frase."""
         subject = ""
         found_det = False
         found_pron = False
@@ -57,6 +55,7 @@ class SyntaxNew:
         return subject.strip()
 
     def find_predicate(self, word_list, grammar_list):
+        """Encontra o predicado de uma frase."""
         found_verb = False
         predicate = []
         for word, grammar in zip(word_list, grammar_list):
@@ -67,6 +66,7 @@ class SyntaxNew:
         return ' '.join(predicate)
 
     def find_verbal_complement(self, word_list, grammar_list):
+        """Encontra o complemento verbal de uma frase."""
         found_verb = False
         object_words = []
         for word, grammar in zip(word_list, grammar_list):
@@ -79,7 +79,8 @@ class SyntaxNew:
                     object_words[-1] += " " + word
         return ' '.join(object_words)
 
-    def modify_voz_passiva(self, sujeito, predicado, verbo, complemento_verbal):
+    def modify_with_voz_passiva(self, sujeito, predicado, verbo, complemento_verbal):
+        """Retorna uma nova frase por meio da voz passiva."""
         # Converte todas as palavras para minúsculas
         sujeito = sujeito.lower()
         predicado = predicado.lower()
@@ -95,17 +96,29 @@ class SyntaxNew:
             if palavra in sujeito_list:
                 sujeito_list.remove(palavra)
         sujeito = " ".join(sujeito_list)
+
         # Adiciona a preposição "por" antes do sujeito
-        #sujeito_com_preposicao = f"pela {sujeito}"
-        # sujeito_sem_det = sujeito.split(' ', 1)[-1]  # Remove o determinante do sujeito
         sujeito_com_preposicao = f"pela {sujeito}"
 
-        # Forma a nova sentença
-        nova_sentenca = f"{complemento_verbal} {verbo_passivo} {sujeito_com_preposicao}".capitalize()
-        return nova_sentenca
+        new_sentence = f"{complemento_verbal} {verbo_passivo} {sujeito_com_preposicao}".capitalize()
+        return new_sentence
+  
+    def modify_with_synonyms(self, word_list):
+        """Retorna uma nova frase a partir da original substituindo algumas palavras por sinônimos."""
+        modified_text = []
+        for word in word_list:
+            if word.isalpha():
+                synonyms = wordnet.synsets(word, lang='por')
+                if synonyms:
+                    new_word = random.choice(synonyms).lemmas(lang='por')[0].name()
+                    if new_word != word:
+                        modified_text.append(new_word)
+                        continue
+            modified_text.append(word)
+        return modified_text
 
     def get_participle(self, verb):
-        # Aqui define a conjugação para o particípio de cada verbo que deseja suportar
+        """Define a conjugação para o particípio de cada verbo que deseja suportar"""
         participle = {
         
             "ou": "ado",
@@ -124,24 +137,9 @@ class SyntaxNew:
         # Retorna o particípio encontrado ou o próprio verbo
         return verb
 
-    def modify_with_synonyms(self, word_list):
-        modified_text = []
-        for word in word_list:
-            if word.isalpha():
-                synonyms = wordnet.synsets(word, lang='por')
-                if synonyms:
-                    new_word = random.choice(synonyms).lemmas(lang='por')[0].name()
-                    if new_word != word:
-                        modified_text.append(new_word)
-                        continue
-            modified_text.append(word)
-        return modified_text
-
-    def change_word_order(self, word_list):
-        random.shuffle(word_list)
-        return word_list
 
     def execute(self, tokens):
+        """Realiza a execução da análise sintática através de uma GLC e cria variações da frase caso a frase original seja aceita."""
         self.tokens = tokens
         list_of_sentences = [] # lista vazia para armazenar as frases modificadas e a original
         grammar_list = [token.grammar for token in self.tokens]
@@ -150,6 +148,7 @@ class SyntaxNew:
         grammar = self.my_grammar()
         parser = ChartParser(grammar)
 
+        print(colored(f"🌳 Gerando árvore sintática...","blue"))
         tree = self.display_tree(parser, grammar_list)
         if tree:
             print(colored("✅ Análise sintática concluída!", "green"))
@@ -157,52 +156,51 @@ class SyntaxNew:
             predicado = self.find_predicate(word_list, grammar_list)
             complemento_verbal = self.find_verbal_complement(word_list, grammar_list)
 
-            print(f"\nSujeito: {sujeito}")
-            print(f"Predicado: {predicado}")
-            if 'AUX' in grammar_list:
-                print(f"Verbo Auxiliar: {word_list[grammar_list.index('AUX')]}")
-            if 'VERB' in grammar_list:
-                print(f"Verbo: {word_list[grammar_list.index('VERB')]}")
-            if 'VERB' not in grammar_list:
-                pass
-            print(f"Complemento: {complemento_verbal}")
+            # print(f"\nSujeito: {sujeito}")
+            # print(f"Predicado: {predicado}")
+            # if 'AUX' in grammar_list:
+            #     print(f"Verbo Auxiliar: {word_list[grammar_list.index('AUX')]}")
+            # if 'VERB' in grammar_list:
+            #     print(f"Verbo: {word_list[grammar_list.index('VERB')]}")
+            # if 'VERB' not in grammar_list:
+            #     pass
+            # print(f"Complemento: {complemento_verbal}")
 
-            # Método 1 de modificação (voz passiva)
-            print("\nMétodo 1 de modificação(voz passiva):")
+
+            print(colored(f"✍️  Gerando variações da frase original...","blue"))
+
             modified_order_1 = []
             if 'VERB' in grammar_list:
-                modified_order_1.append(self.modify_voz_passiva(sujeito, predicado, word_list[grammar_list.index('VERB')], complemento_verbal))
+                modified_order_1.append(self.modify_with_voz_passiva(sujeito, predicado, word_list[grammar_list.index('VERB')], complemento_verbal))
             else:
-                modified_order_1.append(self.modify_voz_passiva(sujeito, predicado, word_list[grammar_list.index('AUX')], complemento_verbal))
-            print(colored("Texto modificado 1:", "yellow"), ' '.join(modified_order_1[0].split()))
+                modified_order_1.append(self.modify_with_voz_passiva(sujeito, predicado, word_list[grammar_list.index('AUX')], complemento_verbal))
+            print(colored("* Texto modificado 1:", "blue"), ' '.join(modified_order_1[0].split()))
 
             # Método 2 de modificação (sinônimos)
-            print("\nMétodo 2 de modificação (sinônimos):")
             modified_text = self.modify_with_synonyms(word_list)
             if 'VERB' in grammar_list:
                 modified_text[grammar_list.index('VERB')] = word_list[grammar_list.index('VERB')]
             else:
                 modified_text[grammar_list.index('AUX')] = word_list[grammar_list.index('AUX')]
             modified_order_2 = ' '.join(modified_text)
-            print(colored("Texto modificado 2:", "magenta"), modified_order_2)
+            print(colored("* Texto modificado 2:", "blue"), modified_order_2)
 
             # Método 3, alteração da ordem das palavras (voz passiva e sinônimos)
-            print("\nMétodo 3, alteração da ordem das palavras (voz passiva e sinônimos):")
             modified_order_3 = []
             if 'VERB' in grammar_list:
-                modified_order_3.append(self.modify_voz_passiva(sujeito, predicado, word_list[grammar_list.index('VERB')], complemento_verbal))
+                modified_order_3.append(self.modify_with_voz_passiva(sujeito, predicado, word_list[grammar_list.index('VERB')], complemento_verbal))
             else:
-                modified_order_3.append(self.modify_voz_passiva(sujeito, predicado, word_list[grammar_list.index('AUX')], complemento_verbal))
+                modified_order_3.append(self.modify_with_voz_passiva(sujeito, predicado, word_list[grammar_list.index('AUX')], complemento_verbal))
             modified_order_with_synonyms = self.modify_with_synonyms(modified_order_3[0].split())
-            print(colored("Texto modificado 3:", "blue"), ' '.join(modified_order_with_synonyms))
+            print(colored("* Texto modificado 3:", "blue"), ' '.join(modified_order_with_synonyms))
 
             # Lista com os resultados dos métodos
             list_of_sentences.append(' '.join(modified_order_1))
             list_of_sentences.append(modified_order_2)
             list_of_sentences.append(' '.join(modified_order_with_synonyms))
-            print (list_of_sentences)
+
             return list_of_sentences
 
         else:
-            print(colored("⚠ Análise sintática finalizada com erro.", "red"))
+            print(colored("❌ Análise sintática finalizada com erro.", "red"))
             return False
